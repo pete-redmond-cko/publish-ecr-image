51728e4ea34b810a5a2e1943ff1955d4b12afee5
Peter.Redmond <peter.redmond@checkout.com>
2019-11-29 16:31:02 +0000

    fix packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { execSync } = require('child_process');
+
+function run(cmd, options = {}) {
+    if (!options.hide) {
+        console.log(`$ ${cmd}`);
+    }
+    return execSync(cmd, {
+        shell: '/bin/bash',
+        encoding: 'utf-8',
+        env: {
+            ...process.env,
+            AWS_ACCESS_KEY_ID,
+            AWS_SECRET_ACCESS_KEY,
+        },
+    });
+}
 
 try {
   const imageTag = core.getInput('image-tag');
   console.log(`building image with tag -> ${imageTag}`);
   core.setOutput("message", `building image with tag -> ${imageTag}`);
+  
+  run(`docker build -t ${imageTag} .`)
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
